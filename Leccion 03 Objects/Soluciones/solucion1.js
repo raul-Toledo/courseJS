ce497e7d2eecc3f056069d4abb25b5f61a06393f
Raul Toledo <ratp82@gmail.com>
2025-09-07 16:09:19 -0600

    Update crearTablero to use new character names

--- Leccion 03 Objects/Soluciones/solucion1.js
+++ Leccion 03 Objects/Soluciones/solucion1.js
@@ -1,7 +1,8 @@
 function crearTablero() {
     return {
-        "Percy": 300,
-        "Annabeth": 280,
-        "Grover": 270
+        "Harry": 300,
+        "Luna": 280,
+        "Cedric": 270
     };
 }
+
